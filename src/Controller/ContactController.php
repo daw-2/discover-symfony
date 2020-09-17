2d4616cf53d44ac094f0bf3dc7ac4fcd07949715
Matthieu Mota <matthieumota@gmail.com>
2020-09-17 15:28:35 +0200

    End form

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -6,6 +6,8 @@ use App\Form\ContactType;
 use App\Model\Contact;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
@@ -13,7 +15,7 @@ class ContactController extends AbstractController
     /**
      * @Route("/contact", name="contact")
      */
-    public function index(Request $request)
+    public function index(Request $request, MailerInterface $mailer)
     {
         $contact = new Contact();
         $form = $this->createForm(ContactType::class, $contact);
@@ -26,6 +28,15 @@ class ContactController extends AbstractController
                 'success',
                 'Bonjour '.$contact->name.', nous avons recu votre demande'
             );
+
+            // Envoi de l'email...
+            $email = (new Email())
+                ->from($contact->email)
+                ->to('admin@monsite.com')
+                ->subject('Demande de contact')
+                ->text($contact->name.' a fait une demande : '.$contact->getMessage());
+
+            $mailer->send($email);
 
             return $this->redirectToRoute('contact');
         }
